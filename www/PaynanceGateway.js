e5b497e7192fdcec13b99ce4a219284ba1cd87c9
MacMini <cszasz@gmail.com>
2025-06-21 15:24:30 +0200

    Update PaynanceGateway.js

--- www/PaynanceGateway.js
+++ www/PaynanceGateway.js
@@ -1,6 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.openGateway = function (params, success, error) {
-    alert('Opening Paynance Gateway with params: ' + JSON.stringify(params));
     exec(success, error, 'PaynanceGateway', 'openGateway', [params]);
 };
